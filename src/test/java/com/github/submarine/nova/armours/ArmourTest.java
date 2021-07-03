d0e68b771b26a801a64d9acf6c90d1d3fbc35ab6
1submarine <submarine31@yandex.com>
2021-07-03 17:37:54 -0400

    Refactored ArmourTest.java

--- src/test/java/com/github/submarine/nova/armours/ArmourTest.java
+++ src/test/java/com/github/submarine/nova/armours/ArmourTest.java
@@ -3,26 +3,22 @@ package com.github.submarine.nova.armours;
 import org.junit.jupiter.api.Test;
 
 class ArmourTest {
+	private final int value = 10;
+	private final Armour local = new Armour(this.value);
 
 	@Test
-	void getValue() {
-		assert new Armour(10).getValue() == 10;
-	}
+	void getValue() { assert this.local.getValue() == this.value; }
 
 	@Test
-	void getIntegrity() {
-		assert new Armour(10).getIntegrity() == 100;
-	}
+	void getIntegrity() { assert this.local.getIntegrity() == 100; }
 
 	@Test
-	void getResist() {
-		assert new Armour(10).getResist() == 10;
-	}
+	void getResist() { assert this.local.getResist() == this.value; }
 
 	@Test
 	void reduceIntegrity() {
-		Armour local = new Armour(10);
-		local.reduceIntegrity(10);
+		Armour local = new Armour(this.value);
+		local.reduceIntegrity(this.value);
 		assert local.getIntegrity() == 90;
 	}
 }
